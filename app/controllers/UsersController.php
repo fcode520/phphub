23504eb63897114caee638e5cb954402f1ff2bae
fcode <fcode520@gmail.com>
2016-03-22 14:31:09 +0800

    skill

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -33,7 +33,7 @@ class UsersController extends \BaseController
         $topics = Topic::whose($user->id)->recent()->paginate(6);
         $favoritetopics = $user->favoriteTopics()->paginate(6);
         $replies = Reply::whose($user->id)->recent()->limit(10)->get();
-        $skill=$resume->skill()->first()->skill;
+        $skill=Skill::where('id','=',$resume->skill_id)->first()->skill;
         if(!is_null($resume)){
             $projects=$resume->userproject()->get();
             return View::make('usersinfo.show', compact('user', 'topics', 'replies','resume','projects','favoritetopics','skill'));
