faace76c8bebae5ff232f4bfe4f376af9ac01f80
fcode <fcode520@gmail.com>
2016-03-22 13:34:17 +0800

    skill

--- app/views/account/setting.blade.php
+++ app/views/account/setting.blade.php
@@ -28,9 +28,9 @@
                                 <span class="col-sm-6"><label>注册时间</label>：{{$resume->user->created_at}}</span></p>
 
                             <p><span class="col-sm-6"><label>QQ</label>：{{$resume->qq}}</span><span class="col-sm-6"><label>工作技能</label>：
-                            @if(isset($resume->skill->skill))
-                            {{$resume->skill->skill}}
-                            @endif
+                            {{--@if(isset($resume->skill()->skill))--}}
+                            {{--{{$resume->skill()->skill}}--}}
+                            {{--@endif--}}
                             </span></p>
                         </di>
                         <div class="clearfix"></div>
