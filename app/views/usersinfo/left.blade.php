b6a5eb663a15e783b81bee13a05bcc307b00eb54
fcode520 <fcode520@gmail.com>
2016-03-25 23:26:48 +0800

    userid

--- app/views/usersinfo/left.blade.php
+++ app/views/usersinfo/left.blade.php
@@ -38,9 +38,9 @@
            <ul>
                <li><span>{{$user->getTopicsups()}}</span><p>赞</p></li>
                {{--<li><span>{{$fans[0]}}</span><p>关注</p></li>--}}
-               <li><span>{{$fans[0]}}</span><p><a href="{{route('ufocus',$currentUser->id)}}">关注</a></p></li>
+               <li><span>{{$fans[0]}}</span><p><a href="{{route('ufocus',$user->id)}}">关注</a></p></li>
                {{--<li><span>{{$fans[1]}}</span><p>粉丝</p></li>--}}
-               <li><span>{{$fans[1]}}</span><p><a href="{{route('fans',$currentUser->id)}}">粉丝</a></p></li>
+               <li><span>{{$fans[1]}}</span><p><a href="{{route('fans',$user->id)}}">粉丝</a></p></li>
            </ul>
          </div>
          <p class="new-follow"><a id="Focus" data="{{$user->id}}" class="btn btn-success" href="#">{{$fans[2]?'取消关注':'关注'}}</a></p>
