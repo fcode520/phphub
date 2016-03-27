8789d0937543eaedb2098de05398877ade79e953
fcode520 <fcode520@gmail.com>
2016-03-27 17:43:35 +0800

    修改个人简介 文字大小和颜色

--- app/views/usersinfo/user_summary.blade.php
+++ app/views/usersinfo/user_summary.blade.php
@@ -2,7 +2,7 @@
         <h2 class="title">个人简介：</h2>
         <div class="new-personal-center-con">
         @if(isset($resume))
-            {{$resume->summary}}
+            <p>{{$resume->summary}}</p>
             @else
             {{'尚未完善资料'}}
         @endif
