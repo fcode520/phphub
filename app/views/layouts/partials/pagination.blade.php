d77782dec37dabe857fdf6966cbb1aca0a169e9c
fcode520 <fcode520@gmail.com>
2016-04-13 22:48:47 +0800

    分页

--- app/views/layouts/partials/pagination.blade.php
+++ app/views/layouts/partials/pagination.blade.php
@@ -1,32 +1,55 @@
 
 
 @if ($paginator->getLastPage() > 1)
-
-    <div class="change-page">
-        <p>
-
-            <a href="{{ ($paginator->getCurrentPage() == 1) ? '#' : $paginator->getUrl(1) }}" class="item{{ ($paginator->getCurrentPage() == 1) ? ' disabled' : '' }}">
-                <i class="icon left arrow"></i>
+          <nav role="navigation">
+              <ul class="cd-pagination">
+                <li class="button">
+                <a href="{{ ($paginator->getCurrentPage() == 1) ? '#' : $paginator->getUrl($paginator->getCurrentPage()-1) }}" class="{{ ($paginator->getCurrentPage() == 1) ? ' disabled' : '' }}">
                 上一页
-            </a>
+                </a>
+                </li>
+               <?php $PrintSize=3;?>
 
-        @for ($i = 1; $i <= $paginator->getLastPage(); $i++)
-            <a href="{{ $paginator->getUrl($i) }}" class="item{{ ($paginator->getCurrentPage() == $i) ? ' active' : '' }}">
-                {{ $i }}
-            </a>
-        @endfor
-            @if($paginator->getCurrentPage()==$paginator->getLastPage())
-                <?php $page=$paginator->getLastPage() ?>
+                @if($paginator->getLastPage()>$paginator->getCurrentPage()+$PrintSize)
+                <?php $pageMax=$paginator->getCurrentPage()+$PrintSize;?>
                 @else
-                <?php $page=$paginator->getCurrentPage()+1?>
+                 <?php $pageMax=$paginator->getLastPage();?>
                 @endif
-
-            <a href="{{ $paginator->getUrl($page) }}" class="item{{ ($paginator->getCurrentPage() == $paginator->getLastPage()) ? ' disabled' : '' }}">
-                下一页
-                <i class="icon right arrow"></i>
-            </a>
-
-        </p>
-    </div>
+@if($paginator->getCurrentPage()%4==0)
+<?php $StartPage=$paginator->getCurrentPage();?>
+@else
+<?php $tmpPage=$paginator->getCurrentPage()%4;?>
+<?php $StartPage=$paginator->getCurrentPage()-$tmpPage+1;?>
 
 @endif
+                @for ($i = $StartPage; $i <= $pageMax; $i++)
+                <li>
+                <a href="{{ $paginator->getUrl($i) }}" class="item{{ ($paginator->getCurrentPage() == $i) ? ' current' : '' }}">{{ $i }}</a>
+                </li>
+                @endfor
+@if($pageMax<$paginator->getLastPage())
+                <li>
+                <a href="#" >{{ "..." }}</a>
+                </li>
+                <li>
+                <a href="{{ $paginator->getUrl($paginator->getLastPage()) }}" >{{ $paginator->getLastPage() }}</a>
+                </li>
+@endif
+               @if($paginator->getCurrentPage()==$paginator->getLastPage())
+                          <?php $page=$paginator->getLastPage() ?>
+                          @else
+                          <?php $page=$paginator->getCurrentPage()+1?>
+               @endif
+
+                <li class="button">
+
+                  <a href="{{ $paginator->getUrl($page) }}" class="{{ ($paginator->getCurrentPage() == $paginator->getLastPage()) ? ' disabled' : '' }}">
+                                下一页
+                            </a>
+                </li>
+              </ul>
+            </nav>
+
+
+@endif
+<script src="assets/js/modernizr.js"></script>
